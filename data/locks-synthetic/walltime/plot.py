3d39cd22c9348171000e15e27d59bf28a0d29ff5
baltoli <bsc28@cam.ac.uk>
2017-02-08 14:02:49 +0000

    Get n, mean, stdev as list

--- data/locks-synthetic/walltime/plot.py
+++ data/locks-synthetic/walltime/plot.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
+import sys
+
+def get_mean_stdev(lines):
+    return list(map(lambda s: float(s.strip()), lines[1].split(",")))
+
 if __name__ == "__main__":
-    pass
+    version = str(sys.argv[1])
+    min_threads = int(sys.argv[2])
+    max_threads = int(sys.argv[3])
+    step = int(sys.argv[4])
+    nsort = int(sys.argv[5])
+    for n in range(min_threads, max_threads + 1, step):
+        with open("{}_{}_{}".format(version, n, nsort)) as f:
+            print([n] + get_mean_stdev(f.readlines()))
